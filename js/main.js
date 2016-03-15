f7ab8cf4c270377ef2f50f187e5ad58089c21957
mattkemball <matthewkemball@Matthews-MacBook-Pro.local>
2016-03-15 17:26:11 -0400

    colourer

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $body = $('body');
+var $form = $('form');
+var $input = $('#color-selection');
+var $color = $('.color')
+var $ball = $('.ball')
+
+$form.on('change', function () {
+ $ball.css('background-color', $input.val())
+});
